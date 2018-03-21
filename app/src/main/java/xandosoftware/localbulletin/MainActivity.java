e2740b8c1fb515e2fefb6f4fcffbf67434b059b6
Andy Smith <andrewhuntsmith@gmail.com>
2018-03-21 16:57:02 -0700

    Added some text and a button to mainActivity
    doing all this one step at a time, don't rush me right now

--- app/src/main/java/xandosoftware/localbulletin/MainActivity.java
+++ app/src/main/java/xandosoftware/localbulletin/MainActivity.java
@@ -2,6 +2,7 @@ package xandosoftware.localbulletin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        String boardText;
+        TextView mainText = (TextView) findViewById(R.id.mainText);
+        boardText = mainText.getText().toString();
+        boardText = boardText + " How are you?";
+        mainText.setText(boardText);
     }
 }
