b736280a521888d9efeee07fd3539ae9473fed34
Andy Smith <andrewhuntsmith@gmail.com>
2018-03-22 17:16:42 -0700

    Made a button listener that takes text from the edit text and adds it to the main text

--- app/src/main/java/xandosoftware/localbulletin/MainActivity.java
+++ app/src/main/java/xandosoftware/localbulletin/MainActivity.java
@@ -2,20 +2,35 @@ package xandosoftware.localbulletin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    protected String boardText;
+    protected TextView mainText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        String boardText;
-        TextView mainText = (TextView) findViewById(R.id.mainText);
+        mainText = (TextView) findViewById(R.id.mainText);
         boardText = mainText.getText().toString();
         boardText = boardText + " How are you?";
         mainText.setText(boardText);
+
+        final Button updateButton = (Button) findViewById(R.id.updateButtonId);
+        updateButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                EditText postText = (EditText) findViewById(R.id.postTextId);
+                boardText = boardText + postText.getText();
+                mainText.setText(boardText);
+            }
+        } );
     }
+
+
 }
